a53617a2efe6c155c131a408bdf6c9dc330f802e
Diego Santiago <diegosantiago92@outlook.com>
2025-06-02 23:03:52 -0300

    <Treinando técnicas de páginas no React.js>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Topo from '@/componentes/Topo';
 
 export const metadata = {
   title: "Create Next App",
@@ -18,8 +8,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="pt-br">
+      <body>
+        <Topo/>
         {children}
       </body>
     </html>
